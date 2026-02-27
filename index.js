18516525a5ad719c3abd0e763a490acbbf6c5369
Madhan@01# <mr35@its.jnj.com>
2026-02-27 19:24:31 +0530

    changes for emailservice

--- index.js
+++ index.js
@@ -35,12 +35,16 @@ const authenticateToken = (req, res, next) => {
   next();
 };
 
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
@@ -50,7 +54,6 @@ const transporter = nodemailer.createTransport({
 app.post("/send-email", authenticateToken, async (req, res) => {
   try {
     const { description } = req.body;
-    console.log("Received description:", process.env.EMAIL_USER, process.env.EMAIL_PASS);
 
     if (!description || description.trim() === "") {
       return res.status(400).json({
